9b718f692bf096faa1e3622d4dd6e3a3f78b37b7
Henry Calderon <theceintiant@gmail.com>
2020-03-06 22:56:48 -0800

    Using some more tests.

--- TimeLog/tests.py
+++ TimeLog/tests.py
@@ -19,3 +19,23 @@ class TimeLogTestCase(TestCase):
         #Assert
         self.assertEqual(clas.slug, "test-page")
         self.assertEqual(clas.get_absolute_url(), "/test-page/")
+
+class ClassListViewTests(TestCase):
+    def test_multiple_pages(self):
+        # user = User.objects.create()
+
+        Class.objects.create(class_name="Test Page", class_description="Test description", class_start_date=datetime.date(2020,12,30),class_end_date=datetime.date(2020,12,31))
+        Class.objects.create(class_name="Another Test Page", class_description="Test description", class_start_date=datetime.date(2020,12,30),class_end_date=datetime.date(2020,12,31))
+
+        response = self.client.get('/')
+
+        self.assertEqual(response.status_code, 200)
+
+        responses = response.context['classes']
+        self.assertEqual(len(responses), 2)
+
+        self.assertQuerysetEqual(
+            responses,
+            ['<Class: Test Page>', '<Class: Another Test Page>'],
+            ordered=False
+        )
